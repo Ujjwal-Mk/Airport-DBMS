b196e5b636ebec176222e5878e1fe582c6f3b2d7
Ujjwal-Mk <ujjwalmk16@gmail.com>
2023-11-05 14:08:27 +0530

    dynamic communication logs

--- tab4.py
+++ tab4.py
@@ -49,31 +49,24 @@ def disp(cursor, conn):
                     - {row[1]}
                 ''')
     with st.container():
-        conn1 = mysql.connector.connect(**db_config())
-        cursor1 = conn1.cursor()
-        types=get_types(cursor1)
-        print(types)
+        # conn1 = mysql.connector.connect(**db_config())
+        # cursor1 = conn1.cursor()
         with st.expander('New Message'):
             with st.form("my_form2", clear_on_submit=True):
-                # print('Hi')
                 st.text_input('Message Subject',key='Subject')
                 st.text_input("Message Body",key='body')
-                # st.text_input('Message Type',key='type',placeholder='Notification Or Emergency')
-                radios = st.radio('Message Type',options=types,index=0,key='type')
+                radios = st.radio('Message Type',options=get_types(cursor))
                 st.form_submit_button('Submit',on_click=submitted)
 
-        if 'submitted' in st.session_state and st.session_state.submitted==True:
-            # print(st.session_state)
-            print(f'{radios},{st.session_state.Subject},{st.session_state.body}')
-            try:
-                operate_str=f'INSERT INTO CommunicationLog (MessageType, MessageSubject, MessageBody, SentDate) VALUES ({radios},{st.session_state.Subject},{st.session_state.body},CURRENT_TIMESTAMP);'
-                print('New Message added')
-                cursor1.execute(operate_str)
-                conn1.commit()
-                cursor1.close()
-                conn1.close()
-                exit()
-            except mysql.connector.ProgrammingError as err:
-                print(err)
-            st.success("New message added")
-            reset()
+            if 'submitted' in st.session_state and st.session_state.submitted==True:
+                try:
+                    Subject=str(st.session_state.Subject)
+                    body=str(st.session_state.body)
+                    operate_str=f"INSERT INTO CommunicationLog (MessageType, MessageSubject, MessageBody, SentDate)\
+                                VALUES ('{radios}', '{Subject}', '{body}', CURRENT_TIMESTAMP);"
+                    cursor.execute(operate_str)
+                    conn.commit()
+                except mysql.connector.ProgrammingError as err:
+                    print(err)
+                st.success("New message added")
+                reset()
